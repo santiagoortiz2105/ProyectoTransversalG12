29abb9c913f6da3130deff42829667a0ba3755d8
Lourdes <luliortiz222@gmail.com>
2025-10-15 21:01:18 -0300

    Proyecto Transversal
    Inicialmente, el proyecto contaba con una clase GP12_Universidad que se utilizaba como test de consola para comprobar el correcto funcionamiento del modelo y la conexión a la base de datos. Se modificó esta clase para que ya no ejecute pruebas por consola, sino que inicie directamente la aplicación gráfica

--- GP12_Universidad/src/Vista/GP12_Universidad.java
+++ GP12_Universidad/src/Vista/GP12_Universidad.java
@@ -6,25 +6,9 @@ import Persistencia.AlumnoData;
 import java.time.LocalDate;
 
 public class GP12_Universidad {
-    public static void main(String[] args) {
-        
-        AlumnoData alumnoData = new AlumnoData();
-
-        //Crear un alumno nuevo
-        Alumno alu = new Alumno(43839685, "Ortiz", "Luli", LocalDate.of(2001, 11, 22), false);
-        alumnoData.guardarAlumno(alu);
-        System.out.println("Alumno guardado: " + alu.getIdAlumno());
-
-        // Buscar por id
-        Alumno buscado = alumnoData.buscarAlumno(alu.getIdAlumno());
-        if (buscado != null) {
-            System.out.println("Encontrado: " + buscado.getNombre() + " " + buscado.getApellido());
-        }
-
-        // Mostrar todos
-        System.out.println("Listado de alumnos:");
-        for (Alumno a : alumnoData.obtenerAlumnos()) {
-            System.out.println(a.getIdAlumno() + " - " + a.getNombre() + " " + a.getApellido());
-        }
+   public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new frmMenuPrincipal().setVisible(true);
+        });
     }
 }
